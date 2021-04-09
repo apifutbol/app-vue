9a0f6f5f8a71b7f49a865d2536f24ce4816e89c2
Rage Brahimi <github@rage.al>
2021-04-09 12:54:39 +0100

    Implemention of Vite

--- src/js/app.js
+++ src/js/app.js
@@ -8,10 +8,10 @@ import Framework7Vue, { registerComponents } from 'framework7-vue/bundle';
 
 import 'framework7/framework7-bundle.css';
 
-import './css/icons.css';
-import './css/app.css';
+import '../css/icons.css';
+import '../css/app.css';
 
-import App from './components/app.vue';
+import App from '../components/app.vue';
 
 Framework7.use(Framework7Vue);
 
